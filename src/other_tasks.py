6c2d6104c6a7e8c41237decabfc4a057dfa906b1
Arctiqa <sinitro@yandex.ru>
2023-10-25 15:44:27 +0800

    description redacted

--- src/other_tasks.py
+++ src/other_tasks.py
@@ -1,3 +1,5 @@
+import os
+
 g = ['hello', 'world', 'apple', 'pear', 'banana', 'pop']
 h = ['', 'madam', 'racecar', 'noon', 'level', '']
 k = [2, 3, 5, 7, 11]
@@ -5,8 +7,22 @@ b = [-5, -7, -9, -13]
 d = [3]
 
 
+def path_to_directory(path: str = os.getcwd()) -> dict:
+    """возвращает путь директории в виде словаря {"files": , "folders": }
+    (Перенесено из прошлого урока сюда)"""
+    len_dirs = 0
+    len_files = 0
+    for root, dirs, files in os.walk(path):
+        len_dirs += len(dirs)
+        len_files += len(files)
+    return {"files": len_files, "folders": len_dirs}
+
+
 def matching_letters(wordlist: list[str]) -> list[str]:
-    """функция возвращает список слов, если первая и последняя буква совпадают"""
+    """
+    :param: список слов
+    :return: список слов, если первая и последняя буква совпадают
+    """
     tenet_list = []
     for word in wordlist:
         if word == '':
@@ -17,7 +33,10 @@ def matching_letters(wordlist: list[str]) -> list[str]:
 
 
 def max_nums_multiplication(num_list: list[int]) -> int:
-    """функция возвращает максимально возможное произведение двух чисел из списка"""
+    """
+    :param: список из цифр
+    :return: максимально возможное произведение двух чисел из списка
+    """
     if len(num_list) < 2:
         return 0
     else:
@@ -27,9 +46,11 @@ def max_nums_multiplication(num_list: list[int]) -> int:
         return max(value1, value2)
 
 
+print(path_to_directory())
+print()
 print(matching_letters(g))
 print(matching_letters(h))
-
+print()
 print(max_nums_multiplication(k))
 print(max_nums_multiplication(b))
 print(max_nums_multiplication(d))
