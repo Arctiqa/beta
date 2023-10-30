f0baca296c0a55447f71e95e1cbc3c0df46d3a42
Arctiqa <sinitro@yandex.ru>
2023-10-30 13:14:20 +0800

    add processing.py

--- src/processing.py
+++ src/processing.py
@@ -1,27 +1,25 @@
-# from src.widget import registration_date
-
-dic_list = [{'id': 41428829, 'state': 'EXECUTED', 'date': '2019-07-03T18:35:29.512364'},
-            {'id': 939719570, 'state': 'EXECUTED', 'date': '2018-06-30T02:08:58.425572'},
-            {'id': 594226727, 'state': 'CANCELED', 'date': '2018-09-12T21:27:25.241689'},
-            {'id': 615064591, 'state': 'CANCELED', 'date': '2018-10-14T08:21:33.419441'}]
-
-
 def get_dicts_not_executed(dict_list: list[dict], state: str = 'EXECUTED') -> list[dict]:
-    new_list = []
+    """
+    возвращает список словарей, отсортированных по значению state
+    :param dict_list: [{'id': 'идентификатор', 'state': 'состояние', 'date': 'дата'}]
+    :param state: состояние платежа
+    :return: отсортированный список по значению 'state'
+    """
+    sorted_list = []
     for dict_ in dict_list:
         if dict_['state'] == state:
-            new_list.append(dict_)
-    return new_list
+            sorted_list.append(dict_)
+    return sorted_list
 
 
 def get_dicts_sorted_by_date(dict_list: list[dict], sort_type: bool = True) -> list[dict]:
+    """
+    возвращает отсортированый по убыванию/возрастанию даты список словарей
+    :param dict_list: [{'id': 'идентификатор', 'state': 'состояние', 'date': 'дата'}]
+    :param sort_type: возрастание/убывание
+    :return: отсортированный список по значению 'date'
+    """
     new_list = sorted(dict_list, key=lambda dict_: dict_['date'], reverse=sort_type)
     return new_list
 
 
-print(dic_list)
-print()
-print(get_dicts_sorted_by_date(dic_list))
-print(get_dicts_sorted_by_date(dic_list, False))
-print(get_dicts_not_executed(dic_list, 'CANCELED'))
-print(get_dicts_not_executed(dic_list))
