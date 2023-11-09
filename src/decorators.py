53a02a5bb2cf48a53999920014b9df47b31b5a03
Arctiqa <sinitro@yandex.ru>
2023-11-09 17:05:25 +0800

    reupload decorators.py, test_decorators.py

--- src/decorators.py
+++ src/decorators.py
@@ -5,26 +5,29 @@ from typing import Any, Callable
 
 def log(filename: Any = None) -> Callable:
     """
-    Декоратор проверяет правильный тип входящих в функцию данных int | float
+    Декоратор записывает в лог данные об исполняемой функции и время исполнения функции
     в случае успеха исполняет функцию, записывает в лог успешное исполнение и время исполнения
-    в случае неуспеха выводит в консоль сообщение об ошибке типа данных, записывает в лог время исполнения
-    :param filename: название файла, в который записывается результат исполняемого дествия
-    :return: возвращает результат функции, либо сообщение об ошибке входных данных
+    в случае неуспеха выводит в консоль сообщение об ошибке, записывает в лог ошибку время исполнения
+    :param filename: название логфайла, в который записывается результат исполняемого дествия
+    :return: возвращает результат исполнения функции, либо сообщение об ошибке
     """
+
     def wrapper(func: Callable) -> Any:
         @wraps(func)
-        def inner(*args: tuple[Any]) -> Any:
-            s = tuple(i for i in args if isinstance(i, int | float))
-            if args == s:
+        def inner(*args: tuple[int | float]) -> Any:
+            try:
                 result = func(*args)
                 log_message = f"{datetime.now()} {func.__name__}, ok"
-            else:
-                result = "error: Ошибка типа данных"
+            except Exception as e:
+                result = str(e)
                 log_message = f"{datetime.now()} {func.__name__}," \
-                              f" {result}. Inputs: {args}, {func.__name__}"
-            with open(filename, 'a', encoding='utf-8') as file:
-                file.write(log_message + "\n")
+                              f" error:{result}. Inputs: {args}, {func.__name__}"
+            if filename is None:
                 print(log_message)
+            else:
+                with open(filename, 'a', encoding='utf-8') as file:
+                    file.write(log_message + "\n")
+
             return result
 
         return inner
@@ -33,16 +36,16 @@ def log(filename: Any = None) -> Callable:
 
 
 @log(filename="mylog.txt")
-def my_function(x: int | float, y: int | float) -> int | float:
+def my_function(x: Any, y: Any) -> Any:
     """
-    Складывает два числа
-    :param x: int | float
-    :param y: int | float
+    Складывает два значения
+    :param x: x
+    :param y: y
     :return: сумма
     """
     return x + y
 
-
+#
 # my_function(1, 2)
 # my_function(1, 't')
 # my_function('r', 5)
--- tests/test_decorators.py
+++ tests/test_decorators.py
@@ -2,7 +2,7 @@ from datetime import datetime, timedelta
 
 import pytest
 
-from src.decorators import my_function
+from src.decorators import my_function, log
 
 
 @pytest.fixture
@@ -11,12 +11,15 @@ def log_file():
 
 
 def test_my_function():
-    result = my_function(3, 5)
-    assert result == 8
+    result1 = my_function(3, 5)
+    result2 = my_function(1, 't')
+    assert result1 == 8
+    assert result2 == "unsupported operand type(s) for +: 'int' and 'str'"
 
 
 @pytest.mark.parametrize("args, expected_result", [((1, 2), 3),
-                                                   ((3.5, 5), 8.5)])
+                                                   ((3.5, 5), 8.5),
+                                                   (('r', 't'), 'rt')])  #
 def test_log(log_file, args, expected_result):
     result = my_function(*args)
 
@@ -33,9 +36,8 @@ def test_log(log_file, args, expected_result):
 
 
 @pytest.mark.parametrize("args, expected_result",
-                         [((1, 't'), 'error: Ошибка типа данных'),
-                          (('r', 't'), 'error: Ошибка типа данных'),
-                          (('r', 4), 'error: Ошибка типа данных')
+                         [((1, 't'), "unsupported operand type(s) for +: 'int' and 'str'"),
+                          (('r', 4), 'can only concatenate str (not "int") to str')
                           ])
 def test_log_errors(log_file, args, expected_result):
     result = my_function(*args)
@@ -49,4 +51,17 @@ def test_log_errors(log_file, args, expected_result):
     actual_time = log_contents[:19]
 
     assert test_time == actual_time
-    assert f"{my_function.__name__}, error" in f"{my_function.__name__}, error: Ошибка типа данных" in log_contents
+    assert f"{my_function.__name__}, error" in f"{my_function.__name__}, " \
+                                               f"error:{result}" in log_contents
+
+
+@pytest.mark.parametrize("args, expected_result",
+                         [((1, 2), 3),
+                          ((1, 't'), "unsupported operand type(s) for +: 'int' and 'str'"),
+                          (('r', 4), 'can only concatenate str (not "int") to str')
+                          ])
+def test_filename_is_none(args, expected_result):
+    func = log()(my_function)
+    result = func(*args)
+
+    assert result == expected_result
