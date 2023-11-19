6b69ddd14b4169bc6f45bd636856c2971d3802a2
Arctiqa <sinitro@yandex.ru>
2023-11-19 21:05:21 +0800

    update masks.py

--- src/masks.py
+++ src/masks.py
@@ -1,15 +1,23 @@
+import logging
+
+
+logger = logging.getLogger(__name__)
+
+
 def card_mask(card_number: str) -> str:
     """Функция маскировки номера карты
     :param str card_number: Номер карты
     :return: Маскированный номер
     """
     if len(card_number) != 16 or not card_number.isdigit():
+        logger.error('incorrect card number entry')
         return "номер карты должен состоять из шестнадцати цифр"
     else:
         card_number = card_number.replace(card_number[6:12], "******")
         card = []
         for i in range(4):
             card.append(card_number[(4 * i): (4 * i + 4)])
+        logger.info('card mask has been processed')
         return " ".join(card)
 
 
@@ -19,6 +27,8 @@ def account_mask(card_account: str) -> str:
     :return: Маскированный счет
     """
     if len(card_account) != 20 or not card_account.isdigit():
+        logger.error('incorrect account number entry')
         return 'номер счета должен состоять из двадцати цифр'
     else:
+        logger.info('account mask has been processed')
         return "**" + card_account[-4:]
